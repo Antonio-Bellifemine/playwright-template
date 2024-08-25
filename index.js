9ef3e0cea50e320d51b68b82059c7c133c242e77
Antonio Bellifemine <idealconsultinggroup2016@gmail.com>
2024-08-25 17:47:01 -0400

    add index - add basic nav test examples - add default test timeout to config

---
+++ index.js
@@ -0,0 +1,4 @@
+// Load Chance
+const Chance = require('chance');
+// Instantiate Chance so it can be used
+export const chance = new Chance();
--- tests/regression/basic_navigation.spec.ts
+++ tests/regression/basic_navigation.spec.ts
@@ -1,7 +1,28 @@
-import { test } from 'playwright/test';
+import { test, expect } from 'playwright/test';
+import {generateRandomUser} from '../../types/user.ts'; 
 
-test('basic navigation', async ({page}) => {
+const user = generateRandomUser();
+
+test.beforeEach( async ({page}) => {
     await page.goto('https://demo.realworld.io/#/');
-    await page.waitForTimeout(10000);
-    await page.reload();
+});
+
+test('go to sign in page', async ({page}) => {
+    await page.locator('a').getByText('Sign in').click();
+    await page.getByPlaceholder('Email').fill(user.username);
+    await page.getByPlaceholder('Password').fill(user.email);
+});
+
+test('go to sign up page', async ({page}) => {
+    await page.locator('a').getByText('Sign up').click();
+    await page.getByPlaceholder('Username').fill(user.username);
+    await page.getByPlaceholder('Email').fill(user.email);
+    await page.getByPlaceholder('Password').fill(user.password);
+});
+
+test('go to home page', async ({page}) => {
+    await page.locator('a').getByText('Global Feed');
+    // Explicitly wait for the element to be attached and visible in the DOM
+    const popularTags = page.locator('p').getByText('Popular Tags');
+    await expect(popularTags).toBeVisible();
 });
